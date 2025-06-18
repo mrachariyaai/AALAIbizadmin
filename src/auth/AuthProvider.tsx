274ad91d51e8dfcab68df6fbf0d3d82db64376a5
vinayak <vinayak.vvs>
2025-06-18 18:01:35 +0530

    Added route protection

--- src/auth/AuthProvider.tsx
+++ src/auth/AuthProvider.tsx
@@ -1,12 +1,14 @@
 import { getCurrentUser } from '@aws-amplify/auth';
+import { Hub } from 'aws-amplify/utils';
 import React, { createContext, useContext, useEffect, useState } from 'react';
+
 const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [authLoading, setAuthLoading] = useState(true);
 
-  const checkUser = async () => {
+  const fetchUser = async () => {
     try {
       const currentUser = await getCurrentUser();
       console.log("Current User: ", currentUser);
@@ -19,11 +21,21 @@ export const AuthProvider = ({ children }) => {
   };
 
   useEffect(() => {
-    checkUser();
+    fetchUser();
+    const unsubscribe = Hub.listen('auth', ({ payload }) => {
+      console.log("Auth event received: ", payload);
+      const { event } = payload;
+      if (['signedIn', 'signedOut'].includes(event)) {
+        fetchUser();
+      }
+    });
+
+    return () => unsubscribe();
+
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user, setUser, authLoading, checkUser }}>
+    <AuthContext.Provider value={{ user, setUser, authLoading, fetchUser }}>
       {children}
     </AuthContext.Provider>
   );
